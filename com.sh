e27f6323700303794e9b18ae54dd84649970974e
Anton Nikanorov <anton@antowka.ru>
2016-04-25 18:27:33 +0300

    first commit

---
+++ com.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+docker run -it --rm --name angular2-quickstart -v /home/anton/Desktop/Projects/JS/todo-angular:/usr/src/app -w /usr/src/app -p 3000:3000 -p 3001:3001 todo-angular $1
+docker stop $(docker ps -a -q)
+docker rm $(docker ps -a -q)
+service docker restart
